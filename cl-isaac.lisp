5d68b681c60be8e16255b3f290b4813c59a3681d
the Phoeron <thephoeron@me.com>
2014-04-10 17:40:10 -0400

    Add INIT-SELF-SEED for stronger cryptographic randomization

--- cl-isaac.lisp
+++ cl-isaac.lisp
@@ -58,10 +58,40 @@
                       (random (ash 1 32))))
           (scramble ctx)))))
 
-;; Needs a 'count' parameter, to control depth of self-seeding.
-;; default 1, >3 best for strong randomization
-(defun init-self-seed (&key (is64 nil))
-  "Initialize cryptographically strong self-seed.  If :is64 t, use ISAAC-64 context.")
+(defun init-self-seed (&key (count 1) (is64 nil))
+  "Initialize cryptographically strong self-seed starting with a kernel-seed as first round, and then performing a scramble on the bits and modulus between the resulting values and a new kernel-seed on each additional count. :count n (default: 1), a real number, instructs the self-seed to repeat n times. If :is64 t, use ISAAC-64 context."
+  (let ((ctx (if is64
+                 (handler-case (make-isaac64-ctx) (error () nil))
+                 (make-isaac-ctx))))
+    (cond ((and is64 ctx)
+           (loop for i from 0 below count
+                 when (> i 0)
+                   do (loop for x from 0 below 256
+                            do (progn
+                                 (scramble64 ctx)
+                                 (setf (aref (isaac64-ctx-randrsl ctx) x)
+                                       (mod (aref (isaac64-ctx-randrsl ctx) x)
+                                            (rand-bits-64 (init-kernel-seed :is64 t) 64)))))
+                 when (= i 0)
+                   do (loop for j from 0 below 256
+                            do (setf (aref (isaac64-ctx-randrsl ctx) j)
+                                     (rand-bits-64 (init-kernel-seed :is64 t) 64))))
+           (scramble64 ctx))
+          (ctx
+           (loop for i from 0 below count
+                 when (> i 0)
+                   do (loop for x from 0 below 256
+                            do (progn
+                                 (scramble ctx)
+                                 (setf (aref (isaac-ctx-randrsl ctx) x)
+                                       (mod (aref (isaac-ctx-randrsl ctx) x)
+                                            (rand-bits (init-kernel-seed) 32)))))
+                 when (= i 0)
+                   do (loop for j from 0 below 256
+                            do (setf (aref (isaac-ctx-randrsl ctx) j)
+                                     (rand-bits (init-kernel-seed) 32))))
+           (scramble ctx))
+          (t nil))))
 
 (defun init-null-seed (&key (is64 nil))
   "Initialize null seed, useful for testing but not production.  If :is64 t, use ISAAC-64 context."
@@ -86,6 +116,17 @@
 (defun jenkins-output-64 (filename)
   "Output ISAAC-64 block context to FILENAME using null seed."
   (let ((ctx (init-null-seed :is64 t)))
+    (with-open-file (o filename :direction :output :if-exists :supersede)
+      (loop for i from 0 below 2 do
+            (generate-next-isaac64-block ctx)
+            (loop for j from 0 below 256 do
+                  (format o "~(~16,'0x~)" (aref (isaac64-ctx-randrsl ctx) j))
+                  (if (= 7 (logand j 7)) (terpri o)))))))
+
+#+:x86-64
+(defun test-self-seed-64 (filename)
+  "Output ISAAC-64 block context to FILENAME using null seed."
+  (let ((ctx (init-self-seed :count 3 :is64 t)))
     (with-open-file (o filename :direction :output :if-exists :supersede)
       (loop for i from 0 below 2 do
             (generate-next-isaac64-block ctx)
