0ff99ea50858ec403ea0d6e7caa2404c4648f6c9
the Phoeron <thephoeron@users.noreply.github.com>
2015-06-05 23:06:31 -0400

    Update cl-isaac.lisp
    add sanity-check test

--- t/cl-isaac.lisp
+++ t/cl-isaac.lisp
@@ -2,4 +2,20 @@
 
 (in-package :cl-user)
 
+(defpackage cl-isaac-test
+  (:use cl cl-isaac prove))
+
+(in-package :cl-isaac-test)
+
+;; NOTE: To run this test file, execute `(asdf:test-system :cl-isaac)' in your Lisp.
+
+(plan 5)
+
+(deftest sanity-check
+  (is 2
+      '(+ 1 1)
+      "Sane Lisp system."))
+
+(run-test-all)
+
 ;; EOF
