bd479257cf2da4af2853cd86e8c5ebfbd2fa600e
the Phoeron <thephoeron@me.com>
2015-06-26 19:36:15 -0400

    add Self-Seed Context tests for ISAAC32 and ISAAC64 algorithms

--- t/cl-isaac.lisp
+++ t/cl-isaac.lisp
@@ -9,7 +9,9 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-isaac)' in your Lisp.
 
-(plan 1)
+(plan 2)
+
+;; Sanity Test
 
 (deftest sanity-check
   (is (+ 1 1)
@@ -21,6 +23,26 @@
   (is (length "four")
       4
       "Length: (length \"four\") => 4."))
+
+;; Self-seed Context Tests
+
+(defparameter *ssctx32* (isaac:init-self-seed))
+
+(defparameter *ssctx64* (isaac:init-self-seed :is64 t))
+
+(deftest self-seed
+  (is-type (type-of *ssctx32*)
+           'cl-isaac:isaac-ctx
+           "*SSCTX32* has a type of ISAAC-CTX.")
+  (is (integer-length (isaac:rand32 *ssctx32*))
+      32
+      "Generated a random 32-bit integer with RAND32 on *SSCTX32*.")
+  (is-type (type-of *ssctx64*)
+           'cl-isaac:isaac64-ctx
+           "*SSCTX64* has a type of ISAAC64-CTX.")
+  (is (integer-length (isaac:rand64 *ssctx64*))
+      64
+      "Generated a random 64-bit integer with RAND64 on *SSCTX64*."))
 
 (run-test-all)
 
