b001908899a5ad8246e16d562f0305717adb47ef
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2023-06-19 10:37:16 -0400

    Add NIL assigned dynamic variables for ISAAC32 and ISAAC64 context fixtures to test suite package.

--- test/package.lisp
+++ test/package.lisp
@@ -1,4 +1,12 @@
 (in-package :cl-user)
 
 (defpackage cl-isaac/test
-  (:use cl cl-isaac parachute))
+  (:use cl cl-isaac parachute)
+  (:export #:*isaac32-ctx*
+           #:*isaac64-ctx*))
+
+(in-package :cl-isaac/test)
+
+(defparameter *isaac32-ctx* nil)
+
+(defparameter *isaac64-ctx* nil)
