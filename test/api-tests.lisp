81d499e7c4da6b71c95acf87f949bf34febbb733
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2023-06-20 17:20:58 -0400

    Fix tests relying on SKIP-ON macro which apparently isn't recursive

--- test/api-tests.lisp
+++ test/api-tests.lisp
@@ -2,18 +2,19 @@
 
 (define-test (cl-isaac api)
   (define-test cl-random-seed
-    (is (isaac:init-common-lisp-random-seed))
-    (is (isaac:init-common-lisp-random-seed :is64 t))
+    (true (isaac:init-common-lisp-random-seed))
+    (true (isaac:init-common-lisp-random-seed :is64 t))
     (of-type isaac:isaac-ctx (isaac:init-common-lisp-random-seed))
     (of-type isaac:isaac64-ctx (isaac:init-common-lisp-random-seed :is64 t)))
-  (skip-on (windows) "Kernel Seeds and Self Seeds are not currently supported on Windows."
-    (define-test kernel-seed
-      (is (isaac:init-kernel-seed))
-      (is (isaac:init-kernel-seed :is64 t))
+  (define-test kernel-seed
+    (skip-on (windows) "Kernel Seeds are not currently supported on Windows."
+      (true (isaac:init-kernel-seed))
+      (true (isaac:init-kernel-seed :is64 t))
       (of-type isaac:isaac-ctx (isaac:init-kernel-seed))
-      (of-type isaac:isaac64-ctx (isaac:init-kernel-seed :is64 t)))
-    (define-test self-seed
-      (is (isaac:init-self-seed :count 3))
-      (is (isaac:init-self-seed :count 3 :is64 t))
+      (of-type isaac:isaac64-ctx (isaac:init-kernel-seed :is64 t))))
+  (define-test self-seed
+    (skip-on (windows) "Self Seeds are not currently supported on Windows."
+      (true (isaac:init-self-seed :count 3))
+      (true (isaac:init-self-seed :count 3 :is64 t))
       (of-type isaac:isaac-ctx (isaac:init-self-seed :count 3))
       (of-type isaac:isaac64-ctx (isaac:init-self-seed :count 3 :is64 t)))))
